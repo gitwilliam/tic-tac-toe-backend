15b6843c20f8da3c050edf630b88967f36e3113f
Bill Harding <william.paul.harding@gmail.com>
2018-08-08 23:11:17 -0500

    Add endGame function to delete games from user space

--- functions/src/index.ts
+++ functions/src/index.ts
@@ -1,8 +1,19 @@
 import * as functions from 'firebase-functions';
+import * as admin from 'firebase-admin';
 
-// // Start writing Firebase Functions
-// // https://firebase.google.com/docs/functions/typescript
-//
-// export const helloWorld = functions.https.onRequest((request, response) => {
-//  response.send("Hello from Firebase!");
-// });
+admin.initializeApp();
+
+/*
+* This function removes references to the game ID from the users space
+*/
+exports.endGame = functions.database.ref('/games/{gameId}')
+    .onDelete((snapshot, context) => {
+        let promises = []
+        if (snapshot.hasChild('user1')) {
+            promises.push(snapshot.ref.root.child('users').child(snapshot.child('user1').val()).child('game').remove());
+        }
+        if (snapshot.hasChild('user2')) {
+            promises.push(snapshot.ref.root.child('users').child(snapshot.child('user2').val()).child('game').remove());
+        }
+        return Promise.all(promises);
+    });
