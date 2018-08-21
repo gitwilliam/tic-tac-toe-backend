332a36899de9f750e7ecdd0df1eb8c1e84284cb0
Bill Harding <william.paul.harding@gmail.com>
2018-08-20 22:21:07 -0500

    Fix race condition between checkTurn and checkWin

--- functions/src/index.ts
+++ functions/src/index.ts
@@ -34,87 +34,93 @@ exports.joinGame = functions.database.ref('/games/{gameId}/user2')
 /*
 * Update whose turn it is
 */
-exports.checkTurn = functions.database.ref('/games/{gameId}/board/{position}')
+exports.checkTurnAndWin = functions.database.ref('/games/{gameId}/board/{position}')
     .onUpdate((snapshot, context) => {
         const promises = []
+        promises.push(checkWin(snapshot, context).then(_ => {
+            promises.push(checkTurn(snapshot, context));
+        })
+        .catch(e => console.error(e)));
 
-        // set the current "turn" to the other user
-        promises.push(snapshot.after.ref.parent.parent.once('value').then(d => {
-
-            // verify that the user playing should be
-            if (d.child('turn').val() === context.auth.uid) {
-                if (d.child('user1').val() === context.auth.uid) {
-                    promises.push(snapshot.after.ref.parent.parent.update({ turn: d.child('user2').val() }));
-                } else {
-                    promises.push(snapshot.after.ref.parent.parent.update({ turn: d.child('user1').val() }));
-                }
-            } else {
-                console.error("checkTurn: a user was able to try and move out of turn.");
-            }
-        }));
-
-        return Promise.all(promises);
+        return promises;
     });
+
+export function checkTurn(snapshot, context) {
+    const promises = []
+    // set the current "turn" to the other user
+    promises.push(snapshot.after.ref.parent.parent.once('value').then(d => {
+
+        // verify that the user playing should be
+        if (d.child('turn').exists() && d.child('turn').val() === context.auth.uid) {
+            if (d.child('user1').val() === context.auth.uid) {
+                promises.push(snapshot.after.ref.parent.parent.update({ turn: d.child('user2').val() }));
+            } else {
+                promises.push(snapshot.after.ref.parent.parent.update({ turn: d.child('user1').val() }));
+            }
+        } else {
+            console.error("checkTurn: a user was able to try and move out of turn.");
+        }
+    }));
+
+    return Promise.all(promises);
+}
 
 /*
-* Check for a Win or Draw
+* Check for a Win/Loss
 */
-exports.checkWin = functions.database.ref('/games/{gameId}/board/{position}')
-    .onUpdate((snapshot, context) => {
-        const promises = []
-        promises.push(snapshot.after.ref.parent.once('value').then(b => {
-            let win = false; 
-            const board = b.val();
+export function checkWin(snapshot, context) {
+    const promises = []
+    promises.push(snapshot.after.ref.parent.once('value').then(b => {
+        let win = false;
+        const board = b.val();
 
-            // BRUTE FORCE CHECK OF THE ENTIRE BOARD.  This could be optimized
-            // to only check neighbors of $position
-            // check for a horizontal win
-            if (board[0] === board[1] && board [1] === board[2] && board[0] !== "") {
-                win = true;
-            } else if (board[3] === board[4] && board [4] === board[5] && board[3] !== "") {
-                win = true;
-            } else if (board[6] === board[7] && board [7] === board[8] && board[6] !== "") {
-                win = true;
-            
+        // BRUTE FORCE CHECK OF THE ENTIRE BOARD.  This could be optimized
+        // to only check neighbors of $position
+        // check for a horizontal win
+        if (board[0] === board[1] && board[1] === board[2] && board[0] !== "") {
+            win = true;
+        } else if (board[3] === board[4] && board[4] === board[5] && board[3] !== "") {
+            win = true;
+        } else if (board[6] === board[7] && board[7] === board[8] && board[6] !== "") {
+            win = true;
+
             // check for a veritcal win
-            } else if (board[0] === board[3] && board[3] === board[6] && board[0] !== "") {
-                win = true;
-            } else if (board[1] === board[4] && board[4] === board[7] && board[1] !== "") {
-                win = true;
-            } else if (board[2] === board[5] && board[5] === board[8] && board[2] !== "") {
-                win = true;
+        } else if (board[0] === board[3] && board[3] === board[6] && board[0] !== "") {
+            win = true;
+        } else if (board[1] === board[4] && board[4] === board[7] && board[1] !== "") {
+            win = true;
+        } else if (board[2] === board[5] && board[5] === board[8] && board[2] !== "") {
+            win = true;
 
             // check for a diagonal win
-            } else if (board[0] === board[4] && board[4] === board[8] && board[0] !== "") {
-                win = true;
-            } else if (board[2] === board[4] && board[4] === board[6] && board[2] !== "") {
-                win = true;
-            }
+        } else if (board[0] === board[4] && board[4] === board[8] && board[0] !== "") {
+            win = true;
+        } else if (board[2] === board[4] && board[4] === board[6] && board[2] !== "") {
+            win = true;
+        }
 
-            let cat = !win;
-            if (cat) {
-                // check for Cat's Scratch
-                board.forEach(element => {
-                    if (element === "") {
-                        cat = false;
-                    }
-                });
-            }
-
-            if (win || cat) {
-                // delete turn, so that nobody can play
-                promises.push(snapshot.after.ref.parent.parent.child('turn').remove());
-
-                // add winner
-                if (cat) {
-                    promises.push(snapshot.after.ref.parent.parent.child('winner').set('cat'));
-                } else {
-                    promises.push(snapshot.after.ref.parent.parent.child('winner').set(context.auth.uid));
+        let cat = !win;
+        if (cat) {
+            // check for Cat's Scratch
+            board.forEach(element => {
+                if (element === "") {
+                    cat = false;
                 }
+            });
+        }
+
+        if (win || cat) {
+            // delete turn, so that nobody can play
+            promises.push(snapshot.after.ref.parent.parent.child('turn').remove());
+
+            // add winner
+            if (cat) {
+                promises.push(snapshot.after.ref.parent.parent.child('winner').set('cat'));
+            } else {
+                promises.push(snapshot.after.ref.parent.parent.child('winner').set(context.auth.uid));
             }
-        }));
+        }
+    }));
 
-        return Promise.all(promises);
-    });
-
-
+    return Promise.all(promises);
+}
